e2966247af3a84b402c8525232cacf87268eadda
root <1033628403@qq.com>
2018-11-27 18:14:58 +0800

    解决跨域的问题和数据库链接的问题

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 // 数据库链接配置
 module.exports = {
   mysql:{
-    host:'106.12.207.193', // 主机地址
+    host:'127.0.0.1', // 主机地址
     user:'congyang',  // 用户名
     password:'congyang', // 密码
     database: 'blog', // 数据库名称
--- server/index.js
+++ server/index.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser'); // post 时需要
 const express = require('express');
 const app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -10,8 +18,8 @@ app.use(bodyParser.json());
 app.use('/api',routerApi);
 
 // 监听端口
-app.listen('3000');
-console.log('success listen at port:3000...');
+app.listen('3001');
+console.log('success listen at port:3001...');
 
 
 

8ebf903e4f2b2e6518bc48a1dbbf801bdf4ca942
congyang <congyang@didapinche.com>
2018-11-27 15:17:49 +0800

    修改数据库配置

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 // 数据库链接配置
 module.exports = {
   mysql:{
-    host:'10.10.80.22', // 主机地址
+    host:'106.12.207.193', // 主机地址
     user:'congyang',  // 用户名
     password:'congyang', // 密码
     database: 'blog', // 数据库名称
